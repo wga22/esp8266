cd47c5c0be0faaba361cbc37108e04678c4c8a4e
wga22 <wga22@yahoo.com>
2016-06-01 22:36:18 -0400

    flash fixes

--- espurino/landscape_AP.js
+++ espurino/landscape_AP.js
@@ -43,7 +43,7 @@ var flash = require("Flash");
 
 //Global Constants / strings
 var PINOUT = D2;
-var STITLE = "Landscape Timer by Will Allen - V32 (2016-05-22)";
+var STITLE = "Landscape Timer by Will Allen - V33 (2016-06-02)";
 var SURLAPI = 'http://api.wunderground.com/api/13db05c35598dd93/astronomy/q/';
 var HTTP_HEAD = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/><link rel=\"icon\" type=\"image/png\" href=\"http://i.imgur.com/87R4ig5.png\">";
 var HTTP_STYLE = "<style>.rc{fontWeight:bold;text-align:right} .lc{} .c{text-align: center;} div,input{padding:5px;font-size:1em;} input{width:95%;} body{text-align: center;font-family:verdana;} button{border:0;border-radius:0.3rem;background-color:#1fa3ec;color:#fff;line-height:2.4rem;font-size:1.2rem;width:100%;} .q{float: right;width: 64px;text-align: right;} .l{background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAALVBMVEX///8EBwfBwsLw8PAzNjaCg4NTVVUjJiZDRUUUFxdiZGSho6OSk5Pg4eFydHTCjaf3AAAAZElEQVQ4je2NSw7AIAhEBamKn97/uMXEGBvozkWb9C2Zx4xzWykBhFAeYp9gkLyZE0zIMno9n4g19hmdY39scwqVkOXaxph0ZCXQcqxSpgQpONa59wkRDOL93eAXvimwlbPbwwVAegLS1HGfZAAAAABJRU5ErkJggg==\") no-repeat left center;background-size: 1em;}</style>";
@@ -90,6 +90,7 @@ function initializeLightingSystem()
 {
 	setMode("initializing System.", 10000);
 	nPageLoads = 0;
+	readValuesFromFlash();
 	fLightsStarted = false;
 	setPin(false);	//turn off the light
 	startWebserver();
@@ -345,9 +346,9 @@ function getPage(req,res)
 	else  //normal page
 	{
 		var fSomethingChanged = false;
-		if(oUrl && oUrl.query && oUrl.query.z && parseInt(oUrl.query.z, 10) && parseInt(oUrl.query.z, 10).length==5 && parseInt(oUrl.query.z, 10)!=ZIP)
+		if(oUrl && oUrl.query && oUrl.query.z && (oUrl.query.z+"").length ===5 && oUrl.query.z!=ZIP)
 		{
-			ZIP = parseInt(oUrl.query.z, 10);
+			ZIP = oUrl.query.z;
 			fSomethingChanged = true;
 		}
 		if(oUrl && oUrl.query && oUrl.query.d && parseInt(oUrl.query.d, 10)&&durationForLights != parseInt(oUrl.query.d, 10))
@@ -449,7 +450,7 @@ function readValuesFromFlash()
 				var nDur = parseInt(aParts[2],10);
 				if(nDelay > -1 && nDur != durationForLights)
 				{
-					durationForLights = nDelay;
+					durationForLights = nDur;
 				}
 			}
 		}
@@ -478,15 +479,5 @@ function writeValuesToFlash()
 		catch(e){}
 	}
 	return uaArr;
-}
-
-
-function intToUArr(nInt)
-{
-	return [(nInt>>16)&0xff,(nInt>>8)&0xff,nInt&0xff];
-}
-function uArrToInt(uaVal)
-{
-	return ((uaVal[0] * 65536) + (uaVal[1] * 256) + (uaVal[2])); 
 }
 onInit();
