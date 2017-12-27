19f667c9ff0606570d6161f663b131cac0e08698
wga22 <wga22@yahoo.com>
2017-12-27 11:17:34 -0500

    version update

--- espurino/landscape_AP.js
+++ espurino/landscape_AP.js
@@ -90,7 +90,7 @@ const flash = require("Flash");
 
 //Global Constants / strings
 const PINOUT = D2;
-const STITLE = 'IOT Landscape Timer - V39 (2017-7-19)';
+const STITLE = 'IOT Landscape Timer - V40 (2017-12-08)';
 const SURLAPI = 'http://api.wunderground.com/api/13db05c35598dd93/astronomy/q/';
 const HTTP_HEAD = '<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/><link rel=\"icon\" type=\"image/png\" href=\"http://i.imgur.com/87R4ig5.png\">';
 const HTTP_STYLE = '<style>.rc{fontWeight:bold;text-align:right} .lc{} .c{text-align: center;} div,input{padding:5px;font-size:1em;} input{width:95%;} body{text-align: center;font-family:verdana;} button{border:0;border-radius:0.3rem;background-color:#1fa3ec;color:#fff;line-height:2.4rem;font-size:1.2rem;width:100%;} .q{float: right;width: 64px;text-align: right;} .l{background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAALVBMVEX///8EBwfBwsLw8PAzNjaCg4NTVVUjJiZDRUUUFxdiZGSho6OSk5Pg4eFydHTCjaf3AAAAZElEQVQ4je2NSw7AIAhEBamKn97/uMXEGBvozkWb9C2Zx4xzWykBhFAeYp9gkLyZE0zIMno9n4g19hmdY39scwqVkOXaxph0ZCXQcqxSpgQpONa59wkRDOL93eAXvimwlbPbwwVAegLS1HGfZAAAAABJRU5ErkJggg==\") no-repeat left center;background-size: 1em;}</style>';
