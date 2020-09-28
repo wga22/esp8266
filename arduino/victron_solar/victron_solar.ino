ed2eecc3e2d3a602a5c5e67abd0e5091f20053e0
wga22 <wga22@yahoo.com>
2020-09-27 22:56:38 -0400

    beta is ready

--- arduino/victron_solar/victron_solar.ino
+++ arduino/victron_solar/victron_solar.ino
@@ -1,186 +1,216 @@
-// Plot DTH11 data on thingspeak.com using an ESP8266 
-// July 11 2015
+// record data from Victron Solar Controller
+// Sept 26 2020
 // Author: Will Allen
 // www.arduinesp.com 
-#include <DHT.h>
-#include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <WiFiClientSecure.h>
-#include <wificredentials.h>
-#define DHTPIN 2 // what pin we're connected to
- 
-// replace with your channel's thingspeak API key, 
-// http://api.wunderground.com/api/13db05c35598dd93/conditions/q/Australia/Sydney.json
-String apiKey = "NK6HEWTA7BC9ANLD";
-String wundergroundKey = "13db05c35598dd93";
+#include <wifi_credentials.h>
+#define RXPIN 2 // what pin we're connected to
+#define TXPIN 3 // what pin we're connected to
+
+/*
+ * thingspeak: https://thingspeak.com/channels/140150
+ *docs: https://beta.ivc.no/wiki/index.php/Victron_VE_Direct_DIY_Cable
+PID 0xA043      -- Product ID for BlueSolar MPPT 100/15
+FW  119     -- Firmware version of controller, v1.19
+SER#  HQXXXXXXXXX   -- Serial number
+V 13790     -- Battery voltage, mV
+I -10     -- Battery current, mA
+VPV 15950     -- Panel voltage, mV
+PPV 0     -- Panel power, W
+CS  5     -- Charge state, 0 to 9
+ERR 0     -- Error code, 0 to 119
+LOAD  ON      -- Load output state, ON/OFF
+IL  0     -- Load current, mA
+H19 0       -- Yield total, kWh
+H20 0     -- Yield today, kWh
+H21 397     -- Maximum power today, W
+H22 0       -- Yield yesterday, kWh
+H23 0     -- Maximum power yesterday, W
+HSDS  0     -- Day sequence number, 0 to 365
+Checksum  l:A0002000148   -- Message checksum
+
+actual:
+PID  0xA04C
+FW  150
+SER#  HQ19379192M
+V 14140
+I 1340
+VPV 15450
+PPV 31
+CS  3
+MPPT  2
+ERR 0
+LOAD  ON
+IL  800
+H19 20
+H20 11
+H21 91
+H22 2
+H23 12
+HSDS  2
+Checksum  µ:AD7ED00160071
+:AD5ED008605FE
+:ABCED00720C000024
+:ABBED000E068F
+:AADED000800A9
+
+recording:
+V 13790     -- Battery voltage, mV
+I -10     -- Battery current, mA
+VPV 15950     -- Panel voltage, mV
+PPV 0     -- Panel power, W
+CS  5     -- Charge state, 0 to 9
+IL  0     -- Load current, mA
+H22 0       -- Yield yesterday, kWh
+H23 0     -- Maximum power yesterday, W
+
+--FUTURE:
+ERR 0     -- Error code, 0 to 119
+HSDS  0     -- Day sequence number, 0 to 365
+H19 0       -- Yield total, kWh
+H20 0     -- Yield today, kWh
+H21 397     -- Maximum power today, W
+
+
+*/ 
+String apiKey = "0NRCT2ZN3PNTMHUG";
+const int httpsPort = 443;
+const int SERIALRATE = 19200;
 //make sure you at least have a WIFI1 in your credentials file
 
-const int buffer=300;
+const unsigned int MAX_INPUT = 50;
 const char* server = "api.thingspeak.com";
-const char fingerprint[] PROGMEM = "fingerprint";
-const int httpsPort = 443;
-
-WiFiClientSecure sslClient;
 ESP8266WiFiMulti WiFiMulti;
+WiFiClientSecure sslClient;
 
 bool resetWifiEachTime = true;
-bool fTesting =false;     //turns off need for DHT11
-
-DHT dht(DHTPIN, DHT11,20);
 int sleepPerLoop = 60*1000*60;  //1 hr   
- 
+
 void setup() 
 { 
-	Serial.begin(115200);
-	Serial.println("waiting for power to stabilize - 30 secs");
+	Serial.begin(SERIALRATE);
+	consoleWrite("waiting for power to stabilize - 30 secs");
 	delay(1000*10);  //10 secs
   //sslClient.setFingerprint(fingerprint);
-  sslClient.setInsecure();
-	dht.begin();
   setupWIFI();
+}
+
+void consoleWrite(String out)
+{
+  //do nothing for now, serial in use for reading!
 }
 
 void loop() 
 {
   startWIFI();
-  
-  //get temp data
-  float h = dht.readHumidity();
-  float t = dht.readTemperature(true);
-  if (isnan(h) || isnan(t)) 
-  {
-    Serial.println("Failed to read from DHT sensor!");
-  }
-
-  //get local weather data
-  Serial.println("get internet temp");
-  float viennaTemp = getInternetTemp();
 
   //write out the data
-  Serial.println("writeThingSpeak");
-  writeThingSpeak(t,h, viennaTemp);
+  consoleWrite("writeThingSpeak");
+  writeThingSpeak();
   WiFi.disconnect();
-  Serial.print("End of loop...sleeping (mins) ");
-  Serial.println(sleepPerLoop/60000);    
+  consoleWrite("End of loop...sleeping (mins) ");
+  consoleWrite(String(sleepPerLoop/60000));    
   delay(sleepPerLoop);
+  //TODO: deepsleep
   // thingspeak needs minimum 15 sec delay between updates  
 }
 
-
-void setupWIFI()
+void getSolarValuesFromSerial(int *solarValues)
 {
-  char* ssid = "";
-  char* password = "";
-  WiFi.mode(WIFI_STA);
-  #ifdef WIFI1_S
-    ssid = WIFI1_S;
-    password = WIFI1_P;
-    WiFiMulti.addAP(ssid, password);
-    Serial.println(ssid);
-  #endif
-  #ifdef WIFI2_S
-    ssid = WIFI2_S;
-    password = WIFI2_P;
-    WiFiMulti.addAP(ssid, password);
-    Serial.println(ssid);
-  #endif
-  #ifdef WIFI3_S
-    ssid = WIFI3_S;
-    password = WIFI3_P;
-    WiFiMulti.addAP(ssid, password);
-    Serial.println(ssid);
-  #endif
-  #ifdef WIFI4_S
-    ssid = WIFI4_S;
-    password = WIFI4_P;
-    Serial.println(ssid);
-    WiFiMulti.addAP(ssid, password);
-  #endif
-    #ifdef WIFI5_S
-    ssid = WIFI5_S;
-    password = WIFI5_P;
-    Serial.println(ssid);
-    WiFiMulti.addAP(ssid, password);
-  #endif
+  /*
+  recording:
+  V 13790     -- Battery voltage, mV
+  I -10     -- Battery current, mA
+  VPV 15950     -- Panel voltage, mV
+  PPV 0     -- Panel power, W
+  CS  5     -- Charge state, 0 to 9
+  IL  0     -- Load current, mA
+  H22 0       -- Yield yesterday, kWh
+  H23 0     -- Maximum power yesterday, W
+  */
+  solarValues[0] = getBoxOfSerial("V");
+  solarValues[1] = getBoxOfSerial("I");
+  solarValues[2] = getBoxOfSerial("VPV");
+  solarValues[3] = getBoxOfSerial("PPV");
+  solarValues[4] = getBoxOfSerial("CS");
+  solarValues[5] = getBoxOfSerial("IL");
+  solarValues[6] = getBoxOfSerial("H22");
+  solarValues[7] = getBoxOfSerial("H23");
 }
 
-void startWIFI()
+int process_data(String searchString, String rowFromPanel)
 {
-  while(WiFiMulti.run() != WL_CONNECTED) 
+  int res = -1;
+  int posOfVar = rowFromPanel.indexOf(searchString);
+  if(posOfVar > 0)
   {
-      Serial.print(".");
-      delay(1000);
+    String valueDetail = rowFromPanel.substring(posOfVar);
+    valueDetail.trim();
+    res = valueDetail.toInt();
   }
-  Serial.println("connected");
+  return res;
 }
 
-float getInternetTemp()
-{  
-  float out = -1;
-  String willcloud = "willcloud.crabdance.com";
-  //http://willcloud.crabdance.com/temp.php
-  //https://www.theamplituhedron.com/articles/How-to-connect-to-an-SSL-protected-server-with-ESP8266(WiFiClient)/
-  Serial.println("WiFiClientSecure");
-  Serial.println("setFingerprint");
-  String stream="";
-  if (sslClient.connect(willcloud, httpsPort)) 
+int getBoxOfSerial(String searchString)
+{
+  int returnValue = -1;
+  //pull no more than 100 lines looking for this value
+  
+  for(int nLine = 0; nLine < 100 && returnValue==-1; nLine++)
   {
-    Serial.println("connected to willcloud");
-    sslClient.println("GET /temp.php HTTP/1.1");
-    sslClient.println("Host: " + willcloud);
-    sslClient.println("Connection: close");
-    sslClient.println();
+    char input_line[MAX_INPUT];
+    unsigned int input_pos = 0;
+  
+    if (Serial.available () > 0)
+    {
+      char inByte = Serial.read();
  
-     //Wait up to 10 seconds for server to respond then read response
-    int pp=0;
-    for(pp=0; (!sslClient.available()) && (pp<500); pp++)
-    {
-      delay(20);
-    }
-    if(pp==500)
-    {
-      Serial.println("there was an issue pulling from the stream");
-      return out;
-    }
-    int x = 2000;  //limit tries
-    for(; !sslClient.find("temp:") && x>0; x--){} // find the part we are interested in.
-    for(pp=0; (sslClient.available()  && (pp < 1000)); pp++) 
-    {
-      char c = sslClient.read();    
-      if(c==-1)
+      switch (inByte)
       {
-       Serial.println("end of string found");
-        break;
-      }
-      stream+=c;
-    }
-    Serial.print("finished reading: ");
-    Serial.println(stream);
-    sslClient.stop();
-  }
-   else
-  {
-    Serial.print("didnt connect to ");
-    Serial.println(willcloud);
-  }
-  return stream.toFloat();
+        case '\n':   // end of text
+        {
+          input_line [input_pos] = 0;  // terminating null byte
+         
+          // terminator reached! process input_line here ...
+          String row = input_line;
+          returnValue = process_data(searchString, row);
+         
+          // reset buffer for next time
+          input_pos = 0; 
+          break;
+        }
+        case '\r':   // discard carriage return
+          //consoleWrite("nothing");
+          break;
+   
+        default:
+          // keep adding if not full ... allow for terminating null byte
+          if (input_pos < (MAX_INPUT - 1))
+          {
+            input_line[input_pos++] = inByte;
+          }
+          break;
+  
+        }  // end of switch
+    }  // end of incoming data    
+  }//for maxtries
+  return returnValue;
 }
 
 
-void writeThingSpeak(float temp, float humid, float vTemp )
+void writeThingSpeak()
 {
-   if (!fTesting && sslClient.connect(server,httpsPort)) 
+  int solarValues[8];
+  getSolarValuesFromSerial(solarValues);
+  if (sslClient.connect(server,httpsPort)) 
   {  //   "184.106.153.149" or api.thingspeak.com
     String postStr = apiKey;
-           postStr +="&field1=";
-           postStr += String(temp);
-           postStr +="&field2=";
-           postStr += String(humid);
-    if(vTemp!=-1)
+    for(int x=0; x < 8; x++)  //8 fields
     {
-      Serial.println("vienna temp there.");
-      postStr += "&field3=";
-      postStr += String(vTemp);
+      postStr +="&field"+String(x+1)+"=";
+      postStr += String(solarValues[x]);
+      consoleWrite("field"+String(x+1)+": " + String(solarValues[x]));
     }
     postStr += "\r\n\r\n";
  
@@ -194,21 +224,54 @@ void writeThingSpeak(float temp, float humid, float vTemp )
      sslClient.print("\n\n"); 
      sslClient.print(postStr);
 
-     Serial.println(postStr);
-           
- 
-     Serial.print("Temperature: ");
-     Serial.print(humid);
-     Serial.print(" degrees fahrenheit Humidity: "); 
-     Serial.print(humid);
-     Serial.print(" internet temp: "); 
-     Serial.print(vTemp);
-     Serial.println("% send to Thingspeak");
+     consoleWrite(postStr);
   }
   else
   {
-    Serial.print("didnt connect to ");
-    Serial.println(server);
+    consoleWrite("didnt connect to ");
+    consoleWrite(server);
   }
   sslClient.stop();
+}
+
+
+void setupWIFI()
+{
+  char* ssid = "";
+  char* password = "";
+  WiFi.mode(WIFI_STA);
+  #ifdef WIFI1_S
+    ssid = WIFI1_S;
+    password = WIFI1_P;
+    WiFiMulti.addAP(ssid, password);
+  #endif
+  #ifdef WIFI2_S
+    ssid = WIFI2_S;
+    password = WIFI2_P;
+    WiFiMulti.addAP(ssid, password);
+  #endif
+  #ifdef WIFI3_S
+    ssid = WIFI3_S;
+    password = WIFI3_P;
+    WiFiMulti.addAP(ssid, password);
+  #endif
+  #ifdef WIFI4_S
+    ssid = WIFI4_S;
+    password = WIFI4_P;
+    WiFiMulti.addAP(ssid, password);
+  #endif
+    #ifdef WIFI5_S
+    ssid = WIFI5_S;
+    password = WIFI5_P;
+    WiFiMulti.addAP(ssid, password);
+  #endif
+}
+
+void startWIFI()
+{
+  while(WiFiMulti.run() != WL_CONNECTED) 
+  {
+      delay(1000);
+  }
+  consoleWrite("connected");
 }
